582f860448c7eef2ad733c31a2d17f9a051cf529
selene <339698418@qq.com>
2025-08-08 21:35:55 +0800

    fixed : intall sys - locla变量问题

--- archlinux/install_sys.sh
+++ archlinux/install_sys.sh
@@ -8,7 +8,7 @@ BOOT_SIZE="512M"                 # EFI boot partition size
 HOSTNAME="arch-linux"            # System hostname
 TIMEZONE="Asia/Shanghai"         # Timezone (e.g. America/New_York)
 # System locale
-LOCALES=("en_US.UTF-8 zh_CN.UTF-8 zh_TW.UTF-8")
+LOCALES="en_US.UTF-8 zh_CN.UTF-8 zh_TW.UTF-8"
 DEFAULT_LOCALE="en_US.UTF-8"                                 
 ROOT_PASSWORD="1"                # Root user password
 USER_NAME="selene"               # Regular username
